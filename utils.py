8dc1c45b6d0ec7ea03159b9962ebd3cf6e579d28
cyberstockofficial <126938651+cyberstockofficial@users.noreply.github.com>
2023-06-13 19:02:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://cyberurl.me/api'
+    url = f''
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
